c7ce0405c3ff095dc2f83187f3d026ef97416439
Igor Pojar <104471774+igariokpojar@users.noreply.github.com>
2023-05-04 08:41:29 -0500

    Reflection

--- src/exercises/Main.java
+++ src/exercises/Main.java
@@ -1,6 +1,24 @@
 package exercises;
 
+import java.util.PrimitiveIterator;
+
 public class Main {
+    private static void printClassInfo(Class<?> ... classes){
+       for (Class<?> clazz : classes){
+           System.out.println(String.format("class name : %s, class package name : %s",
+                   clazz.getSimpleName(),
+                   clazz.getPackageName()));
+
+           Class<?>[] implementInterface = clazz.getInterfaces();
+           for (Class<?> implementedInterface : implementInterface){
+               System.out.println(String.format("class %s implements : %s",
+                       clazz.getSimpleName(),
+                       implementedInterface.getSimpleName()));
+           }
+           System.out.println();
+           System.out.println();
+       }
+    }
 
     private static class Square implements Drawable {
 
@@ -14,4 +32,9 @@ public class Main {
         int getNumberOfCorners();
     }
 
+   private enum Color{
+        BLUE,
+        RED,
+        GREEN
+   }
 }
