27acd52c6bd6b026e4ddfa01e3c42889a1a0078b
Igor Pojar <104471774+igariokpojar@users.noreply.github.com>
2023-06-09 09:28:49 -0500

    Create A class Actor

---
+++ src/data/Actor.java
@@ -0,0 +1,12 @@
+package data;
+
+public class Actor {
+
+    private final String name;
+    private final String [] knowForMovies;
+
+    public Actor(String name, String[] knowForMovies) {
+        this.name = name;
+        this.knowForMovies = knowForMovies;
+    }
+}
